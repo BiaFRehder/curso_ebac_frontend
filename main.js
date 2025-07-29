6b75f4d90bfc3b731f161f1fd96a7fcefc24f161
BiaFucuda <161872803+BiaFRehder@users.noreply.github.com>
2025-07-29 10:46:18 -0300

    Adicinando exercício de typeScript

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+// Função de multiplicar
+function multiplicar(n1, n2) {
+    return n1 * n2;
+}
+// Função de saudação
+function dizOla(nome) {
+    return `Olá ${nome}!`;
+}
+console.log(multiplicar(4, 5));
+console.log(dizOla('Bia'));
---
+++ main.ts
@@ -0,0 +1,12 @@
+// Função de multiplicar
+function multiplicar(n1: number, n2: number): number {
+    return n1 * n2;
+}
+
+// Função de saudação
+function dizOla(nome: string): string {
+    return `Olá ${nome}!`
+}
+
+console.log(multiplicar(4, 5));
+console.log(dizOla('Bia'));
