7d0c04448f90ac61ae6fbf3c5c550c9d343f24bb
BiaFucuda <161872803+BiaFRehder@users.noreply.github.com>
2025-09-30 15:29:30 -0300

    Atualizando o projeto com styled components

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,12 @@
-import styles from './Hero.module.css'
+import { Hero as EstiloHero } from './styles'
+import { Container } from '../../styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+  <EstiloHero>
+    <Container>
+      <h2>As melhores vagas para tecnologia, design e artes visuais.</h2>
+    </Container>
+  </EstiloHero>
 )
 
 export default Hero
